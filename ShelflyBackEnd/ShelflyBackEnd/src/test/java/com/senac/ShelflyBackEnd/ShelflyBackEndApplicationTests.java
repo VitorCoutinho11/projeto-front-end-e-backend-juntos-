b637bc64d762aca54c87e8940c562e428bea44fe
vitor s. <vitorofficial11@gmail.com>
2025-08-26 11:16:06 -0300

    Criação da estrutura

---
+++ ShelflyBackEnd/ShelflyBackEnd/src/test/java/com/senac/ShelflyBackEnd/ShelflyBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.ShelflyBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelflyBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
